80be9d610b842897240ddb5fdb9c6741652139a1
Jim Dai <jim.dai@autoliv.com>
2017-09-21 15:31:07 -0400

    Add save contents to file, change background color

--- Test/Test.java
+++ Test/Test.java
@@ -5,15 +5,22 @@ import java.awt.*;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 
-public class Test  {
+import java.io.*;
 
+public class Test  {
+	
+		boolean textEditable = true;
+	
     public Test() throws BadLocationException {	
-		String buttonDisplay = "Save";
+		
+		String fileName = "write.txt";
+		
+		Color mygray = new Color(245, 245, 245); 
         JFrame frame = new JFrame();
         DefaultStyledDocument document = new DefaultStyledDocument();
         JTextPane pane = new JTextPane(document);
         JPanel mainPanel = new JPanel();
-        JButton button = new JButton(buttonDisplay);
+        JButton button = new JButton("Save");
         button.setPreferredSize(new Dimension(100, 40));
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         pane.setPreferredSize(new Dimension(200, 200));
@@ -62,27 +69,58 @@ public class Test  {
         document.insertString(0, "Four: success \n", style);
         StyleConstants.setForeground(style, Color.RED);
         document.insertString(0, "Three: error \n", style);
-        document.insertString(0, "Two: error \n", style);
+        //document.insertString(0, "Two: error \n", style);
 
-        StyleConstants.setForeground(style, Color.BLACK);
+        //StyleConstants.setForeground(style, Color.BLACK);
         // add some data to the document
-        document.insertString(0, "One: success \n", style);
+       // document.insertString(0, "One: success \n", style);
 		
 		button.addActionListener(new java.awt.event.ActionListener(){ 
 		  public void actionPerformed(java.awt.event.ActionEvent evt) { 	  
 			//int lines = pane.getLineCount();
-			if (buttonDisplay == "Save"){
-				buttonDisplay = "Edit";
+			if (textEditable){
+				textEditable = false;
+				button.setText("Edit");
+				pane.setBackground(mygray);
 				pane.setEditable(false);
 			} else {
-				buttonDisplay = "Save";
+				textEditable = true;
+				button.setText("Save");
 				pane.setEditable(true);
+				pane.setBackground(Color.white);
 			}
 			
 			try {
-			System.out.println(pane.getDocument().getText(0, pane.getDocument().getLength()) );
+			String content = pane.getDocument().getText(0, pane.getDocument().getLength());
+			System.out.println(content);
 			int i = pane.getDocument().getLength();
 			System.out.println(i);
+			
+			 try {
+            FileWriter fileWriter =
+                new FileWriter(fileName);
+
+            // Always wrap FileWriter in BufferedWriter.
+            BufferedWriter bufferedWriter =
+                new BufferedWriter(fileWriter);
+
+            // Note that write() does not automatically
+            // append a newline character.
+            bufferedWriter.write(content);
+           
+            // Always close files.
+            bufferedWriter.close();
+        }
+        catch(IOException ex) {
+            System.out.println(
+                "Error writing to file '"
+                + fileName + "'");
+            // Or we could just do this:
+            // ex.printStackTrace();
+        }
+			
+			
+			
 			} 
 			catch (BadLocationException ee) {
 				//handle exception
